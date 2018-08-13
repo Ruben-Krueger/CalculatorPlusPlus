fcb274f5379174da0ac4d4b015240b81813e4264
Ruben Krueger <rubenkrueger@macs-macbook-pro.local>
2018-08-13 15:20:51 -0700

    first commit

--- Calculator ++/CalculatorModel.swift
+++ Calculator ++/CalculatorModel.swift
@@ -6,243 +6,100 @@
 //  Copyright © 2018 Ruben Krueger. All rights reserved.
 //
 
+func add(_ number1: Double, number2: Double) -> Double {
+    return number1 + number2
+}
+func subtract(_ number1: Double, number2: Double) -> Double {
+    return number1 - number2
+}
+
+func multiply(_ number1: Double, number2: Double) -> Double {
+    return number1 * number2
+}
+
+func divide(_ number1: Double, number2: Double) -> Double {
+    return number1 / number2
+}
+func negate(_ number: Double) ->Double {
+    return number == 0 ? number : number * -1
+}
+
+
+
 import Foundation
 
 class CalculatorModel {
     
-    //
-    private var total = "0"
+    private var total = 0.0
     
-    private var runningTotal = "0"
+    private var pending: pendingBinaryOperationInfo?
     
-    private var operandPressed = ""
-    
-    private let Operands: Set<String> = ["+","-", "*","/"]
-    
-
-    private func convertButtonToDigit(_ str: String) -> Int {
-        switch str {
-        case "0":
-            return 0
-        case "1":
-            return 1
-        case "2":
-            return 2
-        case "3":
-            return 3
-        case "4":
-            return 4
-        case "5":
-            return 5
-        case "6":
-            return 6
-        case "7":
-            return 7
-        case "8":
-            return 8
-        case "9":
-            return 9
-        default:
-            return -1
-        }
-        
-    }
-    
-    
-    private func performOperation() {
-        var result = 0.0
-        if(operandPressed == "+") {
-            result = Double(total)! + Double(runningTotal)!
-            total = String(result)
-        }
-        else if(operandPressed == "-") {
-            result = Double(total)! - Double(runningTotal)!
-            total = String(result)
-        }
-        else if(operandPressed == "*") {
-            result = Double(total)! * Double(runningTotal)!
-            total = String(result)
-        }
-        else if(Double(runningTotal) != 0){
-            result = Double(total)! / Double(runningTotal)!
-            total = String(result)
-        }
-    }
-    
-    func update(_ button: String) -> String{
-  
-        
-        let digit = convertButtonToDigit(button)
-        if(button == "=") {
-            let temp = Double(total)! + Double(runningTotal)!
-            total = String(temp)
-            print("Button: " + button)
-            print("Running total: " + runningTotal)
-            print("Total: " + total)
-            print("Operand pressed" + operandPressed)
-            
-            
+    var result: Double {
+        get {
             return total
         }
-        else if(button == "+/-" && runningTotal != "0") {
-            let temp = -Double(runningTotal)!
-            runningTotal = String(temp)
-            print("Button: " + button)
-            print("Running total: " + runningTotal)
-            print("Total: " + total)
-            print("Operand pressed" + operandPressed)
-            return runningTotal
-        }
-        else if(button == "%" && runningTotal != "0"){
-            let temp = Double(runningTotal)!/100
-            runningTotal = String(temp)
-            print("Button: " + button)
-            print("Running total: " + runningTotal)
-            print("Total: " + total)
-            print("Operand pressed" + operandPressed)
-            return runningTotal
-        }
-        else if(digit != -1 && operandPressed == "") {
-            if(runningTotal == "0") {
-                runningTotal = button
-            }
-            else {
-                runningTotal += button
-            }
-            print("Button: " + button)
-            print("Running total: " + runningTotal)
-            print("Total: " + total)
-            print("Operand pressed" + operandPressed)
-            return runningTotal
-        }
-        // digit pressed AND previous operator pressed
-        else if(digit != -1 && operandPressed != "") {
-            runningTotal = button
-            performOperation()
-            
-            // reset the operand
-            operandPressed = ""
-            print("Button: " + button)
-            print("Running total: " + runningTotal)
-            print("Total: " + total)
-            print("Operand pressed" + operandPressed)
-            return runningTotal
-        }
-            // operator pressed
-        else if(Operands.contains(button)) {
-            operandPressed = button
-            print("Button: " + button)
-            print("Running total: " + runningTotal)
-            print("Total: " + total)
-            print("Operand pressed" + operandPressed)
-            return runningTotal
-        }
-            // if the decimal point was pressed AND the total does not already contain one
-        else if(button == "." && !total.contains(".")) {
-            runningTotal += button
-            print("Button: " + button)
-            print("Running total: " + runningTotal)
-            print("Total: " + total)
-            print("Operand pressed" + operandPressed)
-            return runningTotal
-        }
-        else if(button == "AC") {
-            total = "0"
-            runningTotal = "0"
-            print("Button: " + button)
-            print("Running total: " + runningTotal)
-            print("Total: " + total)
-            print("Operand pressed" + operandPressed)
-            return total
-        }
-        else {
-            print("Button: " + button)
-            print("Running total: " + runningTotal)
-            print("Total: " + total)
-            print("Operand pressed" + operandPressed)
-            return "0"
-        }
-
-        
-//        let digit = convertButtonToDigit(button)
-//        if(button == "=") {
-//            total += runningTotal
-//            return total
-//        }
-//        if(button == "+/-" && total != "0") {
-//            let temp = -Double(total)!
-//            total = String(temp)
-//            return total
-//        }
-//        else if(button == "%" && total != "0"){
-//            let temp = Double(total)!/100
-//            total = String(temp)
-//            return total
-//        }
-//        else if(digit != -1 && operandPressed == "") {
-//            if(total == "0") {
-//                runningTotal = button
-//            }
-//            else {
-//                runningTotal += button
-//            }
-//            return runningTotal
-//        }
-//        // digit pressed AND previous operator pressed
-//        else if(digit != -1 && operandPressed != "") {
-//            performOperation(button)
-//
-//            // reset the operand
-//            operandPressed = ""
-//            return total
-//        }
-//        // operator pressed
-//        else if(Operands.contains(button)) {
-//            operandPressed = button
-//            return total
-//        }
-//        // if the decimal point was pressed AND the total does not already contain one
-//        else if(button == "." && !total.contains(".")) {
-//            runningTotal += button
-//            return runningTotal
-//        }
-//        else if(button == "AC") {
-//            total = "0"
-//            return total
-//        }
-//        else {
-//            return "0"
-//        }
-        
-        
-
-//        else {
-//            // digit pressed AND previous operator pressed
-//            if(digit != -1 && operandPressed != "") {
-//                performOperation(button)
-//
-//                // reset the operand
-//                operandPressed = ""
-//            }
-//            // operand pressed
-//            else if(Operands.contains(button)) {
-//                operandPressed = button
-//            }
-//            // if the decimal point was pressed AND the total does not already contain one
-//            else if(button == "." && !total.contains(".")) {
-//                total += button
-//            }
-//            else if(button == "AC") {
-//                total = "0"
-//            }
-
-//            return total
-//        }
-        
-        
-        
-   
-
-    
     }
+    
+    private struct pendingBinaryOperationInfo {
+        var binaryFunction: (Double, Double) -> Double
+        var firstOperand: Double
+        
+        
+    }
+    
+    private enum Operations {
+        case Const
+        case UnaryOperation((Double) -> Double)
+        case BinaryOperation((Double, Double) -> Double)
+        case Equals
+        
+    }
+    
+    
+    private var operations: Dictionary<String, Operations> = [
+        "√": Operations.UnaryOperation(sqrt),
+        "AC": Operations.Const,
+        "+": Operations.BinaryOperation(add),
+        "-": Operations.BinaryOperation(subtract),
+        "*": Operations.BinaryOperation(multiply),
+        "/": Operations.BinaryOperation(divide),
+        "±": Operations.UnaryOperation(negate),
+        "=": Operations.Equals,
+    ]
+    
+    private func executePendingBinaryOperation() {
+        if pending != nil {
+            total = pending!.binaryFunction(pending!.firstOperand, total)
+        }
+
+    }
+    
+    func setOperand(operand: Double) {
+        total = operand
+    }
+    
+    func performOperation(_ symbol: String) {
+        if let operation = operations[symbol] {
+            switch operation {
+            case .Const:
+                total = 0
+                if pending != nil {
+                    pending?.firstOperand = 0
+                }
+                break
+            case .UnaryOperation(let function):
+                total = function(total)
+                break
+            case .BinaryOperation(let function):
+                executePendingBinaryOperation()
+                pending = pendingBinaryOperationInfo(binaryFunction: function, firstOperand: total)
+                break
+            case.Equals:
+             executePendingBinaryOperation()
+                
+            }
+        }
+    }
+    
+
 }
--- Calculator ++/ViewController.swift
+++ Calculator ++/ViewController.swift
@@ -11,15 +11,51 @@ import UIKit
 class ViewController: UIViewController {
     
     // where the result is displayed
-    @IBOutlet weak var resultField: UILabel!
+    @IBOutlet weak var displayField: UILabel!
     
     // model for calculator
     private let calculator = CalculatorModel()
     
+    private var constants = ["𝛑": 3.14]
     
-    @IBAction func buttonPressed(_ sender: UIButton) {
-        let button = sender.currentTitle
-        resultField.text = calculator.update(button!)
+    private var userTyping = false
+    
+
+    private var displayValue: Double {
+        get {
+            return Double(displayField.text!)!
+        }
+        set {
+            displayField.text = String(newValue)
+        }
+    }
+    
+    @IBAction func digitPressed(_ sender: UIButton) {
+        var digit = sender.currentTitle
+        digit = digit == "𝛑" ? "3.14" : digit
+        if userTyping {
+            let displayText = displayField.text!
+            displayField.text = displayText + digit!
+//            if((displayField.text != "0" || digit != "0")){
+//                displayField.text = displayText + digit!
+//            }
+        }
+        else {
+            displayField.text = digit
+        }
+        userTyping = true
+    }
+    
+    @IBAction func operatorPressed(_ sender: UIButton) {
+        if userTyping {
+            calculator.setOperand(operand: displayValue)
+            userTyping = false
+        }
+        if let symbol = sender.currentTitle {
+            calculator.performOperation(symbol)
+        }
+        displayValue = calculator.result
+        
         
     }
     
